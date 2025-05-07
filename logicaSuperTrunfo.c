8840ee558eb4149da8f001bab701a3a12f32aff0
Willian Santos <wgdisty@gmail.com>
2025-05-07 01:36:52 +0000

    Criei o desafio superTrunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,32 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
+    char* estado1;
+    int populacao1;
+    float area1,pib1,densP1,pibPC1;
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
-
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
-
-    return 0;
+    char* estado2;
+    int populacao2;
+    float area2,pib2,densP2,pibPC2;
+    estado1 = "SaoPaulo";
+    estado2 = "RioJaneiro";
+    populacao1 = 6000;
+    populacao2 = 200000;
+    area1 = 1200;
+    area2 = 1500;
+    pib1 = 300000;
+    pib2 = 30000;
+    
+    densP1 = (populacao1 / area1);
+    pibPC1 = (pib1 / populacao1);
+    
+    densP2 = (populacao2 / area2);
+    pibPC2 = (pib2 / populacao2);
+    
+        if (pib1 > pib2){
+            printf("SAO PAULO GANHOU\n");
+        } else {
+            printf("RIO DE JANEIRO GANHOU\n");
+        }
+    printf("O atributo ultilizado foi o PIB");
 }
